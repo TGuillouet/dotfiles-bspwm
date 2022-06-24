c6d3ccc82175972b9479cc19c42acdd977cadff1
Thomas Guillouet <thomas.guillouet@outlook.com>
2022-06-24 02:08:39 +0200

    Base polybar config + scripts

---
+++ scripts/apply_dots.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp -r ~/.config/bspwm .config/bspwm
+cp -r ~/.config/sxhkd .config/sxhkd
+cp -r ~/.config/fish .config/fish
+cp -r ~/.config/kitty .config/kitty
---
+++ scripts/copy_dots.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp -r ~/.config/bspwm .config
+cp -r ~/.config/sxhkd .config
+cp -r ~/.config/fish .config
+cp -r ~/.config/kitty .config
+cp -r ~/.config/picom .config
+cp -r ~/.config/polybar .config
